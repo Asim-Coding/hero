788e53cf46a73623bd4a3419b9199ac87cfd0181
Asim-Coding <muhammadasim19955@gmail.com>
2022-05-10 17:28:29 +0500

    6 files modified, 7 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,25 @@
-import { Component, VERSION } from '@angular/core';
-import { Hero } from './hero';
+// import { Component, VERSION } from '@angular/core';
+// import { Hero } from './hero';
+
+// @Component({
+//   selector: 'my-app',
+//   templateUrl: './app.component.html',
+//   styleUrls: [ './app.component.css' ]
+// })
+// export class AppComponent  {
+//   title = 'Tour of Heroes';
+//   heroes: Hero[] = [];
+
+
+
+// }
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent {
   title = 'Tour of Heroes';
-  heroes: Hero[] = [];
-
-
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { MessageComponent } from './message/message.component';
 
+import { AppRoutingModule } from './app-routing.module';
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, HeroesComponent,MessageComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessageComponent
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
